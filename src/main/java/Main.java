0849a78934509ae75414b44abebe96d864a49026
ArtemYachmenev <iachmeneow.a@yandex.ru>
2023-09-13 21:54:40 +0500

    add Lesson_7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,10 @@ public class Main {
 
         //счетчик по которому создается двумерный массив
         int count=1;
-        int[][] mas=new int[4][4];
+        //измените размер массива для проверки на его размер
+        Object[][] mas=new Object[4][4];
 
-        //создаем для примера двумерный массив который потом запишется в файл вместе с заголовком
+        //создаем для примера двумерный массив объектов
         //строка двумерного массива
         for(int i = 0; i < mas.length; i++) {
             //столбец двумерного массива
@@ -15,8 +16,40 @@ public class Main {
                 mas[i][j]=count;
                 count++;
                 //выводим массив
-                System.out.println(mas[i][j]);
+                //System.out.println(mas[i][j]);
             }
         }
+
+        //проверка на ошибку в элементе массива, пожалуйста, раскоментируйте чтобы проверить
+        //mas[0][1]="1";
+
+        //вызываем метод и ловим исключения
+        try {
+            firstMethod(mas);
+        } catch (MyArraySizeException e) {
+            System.out.println("Предупреждение! Размер массива не 4х4");
+        } catch (MyArrayDataException e) {
+            System.out.println("Предупреждение! Ошибка в:"+e.i+"x"+e.j);
+        }
+    }
+    public static void firstMethod(Object[][] mas) throws MyArraySizeException, MyArrayDataException {
+        int sum=0;
+        //если массив не размера 4х4
+        if (mas.length!=4||mas[0].length!=4) throw new MyArraySizeException();
+        //проход по массиву
+        for(int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++) {
+                //пытаемся преобразовать объект в число
+                try {
+                    sum+=(int) (mas[i][j]);
+                }
+                //ловим исключение
+                catch (ClassCastException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        System.out.println("sum="+sum);
+
     }
 }
--- src/main/java/MyArrayDataException.java
+++ src/main/java/MyArrayDataException.java
@@ -2,4 +2,12 @@ package main.java;
 
 //выбрасывается если в ячейке массива лежат неверные данные
 public class MyArrayDataException extends Exception{
+    public int i;
+    public int j;
+
+    MyArrayDataException(int i, int j) {
+        this.i = i;
+        this.j = j;
+
+    }
 }
