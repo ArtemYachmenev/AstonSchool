773f4ac50f1978d7187e7585ff1fe1433182c375
ArtemYachmenev <iachmeneow.a@yandex.ru>
2023-09-24 23:49:59 +0500

    add lesson_10

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -1,18 +1,24 @@
 package main.java;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class Box <T extends Fruit> implements Comparable<Box<T>> {
+//я не понял как из этого класса как-нибудь "неявно вытащить" тип дженерика (яблоко или апельсин)
+//была мысль его вытащить и сравнить методы красиво с помощью имплементации Comparable
+//можно ли это как-нибудь сделать чтобы в будущем еще красивее все оформить?
 
-    private List<T> list=new ArrayList<>();
+public class Box <T extends Fruit>  {
+
+    private List<T> list = new ArrayList<>();
+
     private T type;
 
+    //при создании бокса явно указываем тип фруктов в нем
+    public Box(T type) {
+        this.type = type;
+    }
+
     public T getType() {
-        if (this.getList().get(0) instanceof Apple){
-            type= (Apple) type;
-        }
         return type;
     }
 
@@ -21,71 +27,59 @@ public class Box <T extends Fruit> implements Comparable<Box<T>> {
     }
 
     //возвращает массу коробки
-    public float getWeight(){
-        float weight=0;
-        for (T t: list){
-            if (t instanceof Apple){
-                weight+=1.0f;
-            }
-            else {
-                weight+=1.5f;
+    public float getWeight() {
+        float weight = 0;
+        for (T t : list) {
+            if (t instanceof Apple) {
+                weight += 1.0f;
+            } else {
+                weight += 1.5f;
             }
         }
         return weight;
     }
 
     // добавляем в коробку один элемент если добавляемый фрукт и фрукт в коробке разные то выскакивает ошибка
-    public void addFruitInBox(T fruit) {
-        if (list.size() > 1) {
-            if (comparableFruitInBox(fruit)) {
-                try {
-                    throw new Exception("Фрукты разные");
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+    public void addFruitInBox(T fruit)  {
+            if (!compareToTypeFruit(fruit)) {
+
+                System.out.println("Фрукты разные");
             }
+            else {
+            list.add(0, fruit);
         }
-        list.add(0, fruit);
     }
 
     //сравниваем вес
-    public  boolean compare(Box box){
-        return getWeight() - box.getWeight()==0?true:false;
+    public boolean compareWeight(Box box) {
+        return getWeight() - box.getWeight() == 0 ? true : false;
     }
 
-
-    //определяем сопоставимы ли коробки чтобы в них перекладывать фрукты
-    public boolean comparableFruitInBox(Box box) {
-
-//        if (this.list.getClass()==box.getList().getClass()){
-//            return true;
-//        }
-//        return false;
-       // return list.get(0).getClass() != box.getList().get(0).getClass();
-        return false;
-    }
-
-    //определяем сопоставимы ли коробки чтобы в них перекладывать фрукты
-    public boolean comparableFruitInBox(Fruit fruit) {
-        return list.get(0).getClass() != fruit.getClass();
-    }
 
     //перекладывание фруктов, если фрукты разные то выкидывается ошибка
     public void shiftingFruits(Box box) {
-        if (comparableFruitInBox(box)) {
-            System.out.println("Фрукты разные");
-
+        if (!compareToTypeBox(box)) {
+           System.out.println("Фрукты разные");
+            //throw new RuntimeException("Фрукты разные");
         }
-        for (int i = list.size() - 1; i >= 0; i--) {
-            box.getList().add(list.get(i));
-            list.remove(i);
+        else {
+            for (int i = list.size() - 1; i >= 0; i--) {
+                box.getList().add(list.get(i));
+                list.remove(i);
+            }
         }
 
     }
 
-
-    @Override
-    public int compareTo(Box<T> o) {
-        return this.compareTo(o);
+    //определяем сопоставимы ли коробки чтобы в них перекладывать фрукты
+    public boolean compareToTypeBox(Box<T> o) {
+        return this.type==o.getType();
     }
+
+    //определяем сопоставимы ли фрукты
+    public boolean compareToTypeFruit(T fruit) {
+        return this.type.getClass()==fruit.getClass();
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,52 +4,42 @@ public class Main {
     public static void main(String[] args) {
 
         //создание коробок с фруктами и добавление в нех фруктов
-        Box boxApple=new Box();
+        Box boxApple=new Box(new Apple());
         boxApple.addFruitInBox(new Apple());
         boxApple.addFruitInBox(new Apple());
         boxApple.addFruitInBox(new Apple());
 
-        Box boxApple2=new Box();
+        Box boxApple2=new Box(new Apple());
         boxApple2.addFruitInBox(new Apple());
         boxApple2.addFruitInBox(new Apple());
         boxApple2.addFruitInBox(new Apple());
 
-        Box boxOrange=new Box();
+        Box boxOrange=new Box(new Orange());
         boxOrange.addFruitInBox(new Orange());
         boxOrange.addFruitInBox(new Orange());
         boxOrange.addFruitInBox(new Orange());
+
 
         //смотрим массу коробок
         System.out.println("масса коробки с яблоками: "+boxApple.getWeight());
         System.out.println("масса коробки с апельсинами: "+boxOrange.getWeight());
 
         //сравниваем коробки
-        System.out.println("массы коробок одинаковые? "+boxApple.compare(boxOrange));
-        System.out.println("массы коробок одинаковые? "+boxApple.compare(boxApple2));
+        System.out.println("массы коробок одинаковые? boxApple vs boxOrange "+boxApple.compareWeight(boxOrange));
+        System.out.println("массы коробок одинаковые? boxApple vs boxApple2 "+boxApple.compareWeight(boxApple2));
 
         //пересыпаем из одной корзины в другую
         boxApple.shiftingFruits(boxApple2);
         System.out.println("вес коробки из которой пересыпали:"+boxApple.getWeight());
         System.out.println("вес коробки в которую пересыпали:"+boxApple2.getWeight());
 
-        Apple apple=new Apple();
-        System.out.println(apple.getClass());
-        System.out.println(boxApple.getList().getClass().getTypeName().toString());
-        System.out.println(boxApple.compareTo(boxOrange));
         //проверка выкидывания ошибки при пересыпании разных фруктов
-//        try {
-//            boxApple.shiftingFruits(boxOrange);
-//        }
-//        catch (Exception e){
-//            System.out.println(e);
-//        }
+
+        boxApple.shiftingFruits(boxOrange);
 
         //проверка добавления в коробку не того типа фрукта
-//        try {
-//            boxApple.addFruitInBox(new Orange());
-//        }
-//        catch (Exception e){
-//            System.out.println(e);
-//        }
+        boxApple.addFruitInBox(new Orange());
+
+
     }
 }
