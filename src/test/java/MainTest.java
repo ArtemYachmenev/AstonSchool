d4a3e926562676935450e9cf36bed715b9286d18
iachm <iachmeneow.a@yandex.ru>
2023-10-01 22:03:34 +0500

    Lesson_12_junit_5 + maven

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.management.ConstructorParameters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+   static int n,k;
+    @BeforeAll
+    static void prepareData() {
+        n = 5;
+    }
+
+    //проверяем факториал 5, он равен 120
+    //проверяем в мейне что в метод getFactorial передал в переменную к результат 120
+    @Test
+    void main() {
+        getFactorial();
+        assertEquals(120,k);
+    }
+
+    //проверяем что в методе getFactorial результат равен 120
+    @Test
+    void getFactorial() {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        k=result;
+        assertEquals(120,result);
+    }
+}
