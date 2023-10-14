a82a4b28ce3a27ce37dd209ff218dfb5b1c55a68
ArtemYachmenev <iachmeneow.a@yandex.ru>
2023-10-14 21:39:40 +0500

    тест в src/test/java/tests/base/BaseTest

--- src/main/java/Pages/basket/BasketPage.java
+++ src/main/java/Pages/basket/BasketPage.java
@@ -4,7 +4,6 @@ import Pages.base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 
 import java.util.List;
 import java.util.Map;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,5 @@
 package org.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-
-import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) {
         //тест в src/test/java
