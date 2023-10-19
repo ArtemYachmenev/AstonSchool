0d6b343907bb7bd58680f4217fbe4b0bc9cc46b7
ArtemYachmenev <iachmeneow.a@yandex.ru>
2023-10-19 22:47:12 +0500

    теcт в src/test/java/test

--- src/test/java/org/example/Test.java
+++ src/test/java/org/example/Test.java
@@ -1,109 +1,114 @@
 package org.example;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Test {
+        AndroidDriver<AndroidElement> driver=null;
 
-    private static WebDriver driver;
+        @BeforeEach
+    public void initialize() {
+        DesiredCapabilities capabilities = new
+                DesiredCapabilities();
 
-    //перед всеми тестами устанавливаем свойства драйвера
-    @BeforeAll
-    static void setProperty(){
-        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver-win64/chromedriver.exe");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Nexus 5X");
 
-    }
+            //пытался изначально использовать google калькулятор, но не понял как можно вытащить верный
+            //AndroidMobileCapabilityType.APP_ACTIVITY для него, подскажите пожалуйста, как это можно сделать?
+            //в appium нет такого объекта как cal.CalculatorActivity
 
-    //перед каждым тестом устанавливаем драйвер,
-    //разворачиваем окно хрома,
-    //переходим на сайт мтс, ставим неявное ожидание на 5 секунд для загрузки страницы
-    @BeforeEach
-    void setActions(){
-        driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.mts.by/");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-    }
+//        capabilities.setCapability(AndroidMobileCapabilityType
+//                .APP_ACTIVITY,"cal.CalculatorActivity");
 
-    //после каждого теста закрываем драйвер
-    @AfterEach
-    void closeDriver(){
-        driver.quit();
-    }
+            capabilities.setCapability(AndroidMobileCapabilityType
+                    .APP_PACKAGE,"com.miui.calculator");
+            capabilities.setCapability(AndroidMobileCapabilityType
+                    .APP_ACTIVITY,"cal.CalculatorActivity");
 
-    //метод ищет элемент с помощью xpath
-    private WebElement sourseXpath(WebDriver driver, String s){
-        return driver.findElement(By.xpath(s));
-    }
-
-    //метод вставляет в поиск тега button текст
-    private String textToButton(String s) {
-        return "//button[text()=".concat("'").concat(s).concat("'").concat("]");
-    }
-
-    //проверяем что есть блок
-    @org.junit.jupiter.api.Test
-    void enabledBlock(){
-        WebElement block=sourseXpath(driver,"//div[@class='col-12 col-xl-8']");
-        assertEquals(true,block.isEnabled());
-    }
-
-    //проверяем наличие логотипов
-    @org.junit.jupiter.api.Test
-    void enabledImg(){
-        //Хотел написать путь //ul[@src='https://www.mts.by/local/templates/new_design/assets/html/images/pages/index/pay/visa.svg']/li/img
-        //но он не ищется, можно это как-то исправить или по некотором атрибутам не делается поиск?
-        List<WebElement> img=driver.findElements(By.xpath("//section//ul/li/img"));
-        assertAll(() -> assertTrue(img.get(0).isDisplayed()), () -> assertTrue(img.get(1).isDisplayed()),
-                () -> assertTrue(img.get(2).isDisplayed()), () -> assertTrue(img.get(3).isDisplayed()),
-                () -> assertTrue(img.get(4).isDisplayed()), () -> assertTrue(img.get(5).isDisplayed()));
-    }
-
-    //проверяем работу ссылки «Подробнее о сервисе»
-    @org.junit.jupiter.api.Test
-    void clickToLink(){
-        //поиск по этому xpath тоже не работает :/
-        // //a[@href='https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/']
-        sourseXpath(driver,"//a[text()='Подробнее о сервисе']").click();
-        assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/", driver.getCurrentUrl());
-    }
-
-    //Заполняем поля и проверяем работу кнопки «Продолжить»
-    @org.junit.jupiter.api.Test
-    void fullFieldsAndClickButton(){
-        WebElement phone = sourseXpath(driver, "//*[@id=\"connection-phone\"]");
-        WebElement summa = sourseXpath(driver, "//*[@id=\"connection-sum\"]");
-        WebElement email = sourseXpath(driver, "//*[@id=\"connection-email\"]");
-        WebElement nextButton = sourseXpath(driver, textToButton("Продолжить"));
-        phone.sendKeys("297777777");
-        summa.sendKeys("77");
-        email.sendKeys("artem@prikolovich.aston");
-        nextButton.click();
-
-        //перехватываем всплывшее окно
-        WebDriver frame = driver.switchTo().frame(sourseXpath(driver, "//iframe[@class='bepaid-iframe']"));
-
-        //ждем
-        // тут вызывал driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        // но с ним выкидывает пустое значение суммы где ожидалось полное:
-        //Expected :77.00 BYN
-        //Actual   :
-        // может я что-то не так делаю? подскажите пожалуйста в комментах по задаче
+        capabilities.setCapability(MobileCapabilityType.NO_RESET,true);
         try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            driver = new AndroidDriver<>(new
+                    URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+            driver.manage().timeouts().implicitlyWait(10,
+                    TimeUnit.SECONDS);
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
         }
-
-        WebElement text=sourseXpath(frame, "//p[@class='header__payment-amount'][text()=' 77.00 BYN ']");
-        assertEquals("77.00 BYN",text.getText().trim());
     }
-}
+
+    //сложение
+    @org.junit.jupiter.api.Test
+    void plusTest() {
+
+        driver.findElementById("com.miui.calculator:id/btn_3_s").click();
+
+        driver.findElementById("com.miui.calculator:id/btn_plus_s").click();
+        driver.findElementById("com.miui.calculator:id/btn_9_s").click();
+
+        driver.findElementById("com.miui.calculator:id/btn_equal_s").click();
+        assertEquals("= 12", driver.findElementByAndroidUIAutomator("new UiSelector().packageName(\"com.miui.calculator\")." +
+                "resourceId(\"com.miui.calculator:id/result\")").getText());
+
+
+
+
+    }
+
+    //вычитание
+    @org.junit.jupiter.api.Test
+    void minusTest(){
+        driver.findElementById("com.miui.calculator:id/btn_3_s").click();
+
+        driver.findElementById("com.miui.calculator:id/btn_minus_s").click();
+        driver.findElementById("com.miui.calculator:id/btn_9_s").click();
+
+        driver.findElementById("com.miui.calculator:id/btn_equal_s").click();
+        assertEquals("= -6", driver.findElementByAndroidUIAutomator("new UiSelector().packageName(\"com.miui.calculator\")." +
+                "resourceId(\"com.miui.calculator:id/result\")").getText());
+    }
+
+    //умножение
+    @org.junit.jupiter.api.Test
+    void mulTest(){
+        driver.findElementById("com.miui.calculator:id/btn_3_s").click();
+
+        driver.findElementById("com.miui.calculator:id/btn_mul_s").click();
+        driver.findElementById("com.miui.calculator:id/btn_9_s").click();
+
+        driver.findElementById("com.miui.calculator:id/btn_equal_s").click();
+        assertEquals("= 27", driver.findElementByAndroidUIAutomator("new UiSelector().packageName(\"com.miui.calculator\")." +
+                "resourceId(\"com.miui.calculator:id/result\")").getText());
+    }
+
+    //деление
+    @org.junit.jupiter.api.Test
+    void divTest(){
+        driver.findElementById("com.miui.calculator:id/btn_9_s").click();
+        driver.findElementById("com.miui.calculator:id/btn_div_s").click();
+        driver.findElementById("com.miui.calculator:id/btn_3_s").click();
+
+        driver.findElementById("com.miui.calculator:id/btn_equal_s").click();
+        assertEquals("= 3", driver.findElementByAndroidUIAutomator("new UiSelector().packageName(\"com.miui.calculator\")." +
+                "resourceId(\"com.miui.calculator:id/result\")").getText());
+    }
+
+
+    }
