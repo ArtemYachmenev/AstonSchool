0c3f615dbdb002a3cccf97528f93f3d7fae1318b
Artem <iachmeneow.a@yandex.ru>
2023-10-25 12:54:26 +0500

    доб негативный тест

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,30 +5,46 @@ import static org.testng.Assert.*;
 
 public class MainTest {
 
-    //объявлялем n, k. n-число, которое передаетчя в метод
-    //testGetFactorial, k-присваиват результат в этом методе
-    int n,k;
+    //объявляем positive, k. negative negative и -число, которое передаетчя в метод
+    //getFactorial, k и message-присваиват результат в этом методе
+    static int positive,k;
+    static Object negative, message;
+
     @BeforeMethod
     public void setUp() {
-        n = 5;
+        positive = 5;
+        negative = "dfgdfg";
     }
 
     //проверяем факториал 5, он равен 120
     //проверяем в мейне что в метод testGetFactorial передал в переменную к результат 120
+    //позитивный тест
     @Test
-    public void testMain() {
-        testGetFactorial();
+    public void positiveTest() {
+        testGetFactorial(positive);
         assertEquals(120,k);
     }
 
-    //проверяем что в методе getFactorial результат равен 120
+    //негативный тест
     @Test
-    public void testGetFactorial() {
+    void negativeTest() {
+        testGetFactorial(negative);
+        assertEquals("error, no int",message);
+    }
+
+    //проверяем что в методе getFactorial результат равен 120
+
+    public void testGetFactorial(Object data) {
         int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result = result * i;
+        if (!data.getClass().equals(Integer.class)){
+            message="error, no int";
         }
-        k=result;
-        assertEquals(120,result);
+        else {
+            int count = (Integer) data;
+            for (int i = 1; i <= count; i++) {
+                result = result * i;
+            }
+            k = result;
+        }
     }
 }
